7744c81b2cb7fcfd889a819c114ff632ed503a5a
Alessandro Mglx <123805284+AlessandroMglx@users.noreply.github.com>
2025-03-24 17:26:32 +0100

    Add files via upload

---
+++ pastebin-upload.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Warning: Once the content is uploaded, it will not be possible to remove it from the server."
+
+read -e -p "What file do you want to upload? " file
+
+if [ ! -f "$file" ]; then
+  echo "Error: $file does not exist."
+  exit 1
+fi
+
+response=$(curl -s -X POST --data-binary "@$file" "https://pastebin.zampto.net/documents")
+key=$(echo "$response" | grep -oP '(?<="key":")[^"]+')
+
+echo "Your file has been uploaded to: https://pastebin.zampto.net/$key"
